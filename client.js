40bd75e59c0e18186861bfca5ef80b3328e98cab
Nate Schultz <ngs32@drexel.edu>
2022-06-04 21:15:18 -0400

    worked on login logic

--- client.js
+++ client.js
@@ -1,40 +1,70 @@
 const { start } = require('repl');
 var io = require('socket.io-client');
 var readline = require('readline');
+var read_login;
+var read_input;
+
+function login() {
+
+    read_login = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    // prompt user for login credentials
+    read_login.question('User name:', (user) => {
+        read_login.question('Password:', (pass) => {
+            socket.emit("login", {
+                username: user,
+                password: pass
+            })
+            read_login.close();
+        });
+    });
+}
+
+function start_playing() {
+
+    read_input = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    // print output from server
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+    });
+
+    // send command line input to server
+    read_input.on('line', (input) => {
+        socket.emit('message', {msg: input});
+    });
+}
 
 // connect to server
-var socket = io.connect('http://localhost:3000', {reconnect: true});
+var socket = io.connect('http://localhost:3000', {reconnect: false});
+socket.on('error', (error) => {console.log(error);});
 
-var read_input = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-
-var username;
-socket.on('connect', function (socket) {
+socket.on('connect', function (event) {
     console.log('Connected!');
-    read_input.question('User name:', (input) => {
-        username = input;
-    });
+    login();
 });
 
-socket.on('error', (error) => {
-    console.log(error);
-});
+socket.on('disconnect', function (event) {
+    console.log('Disconnected!');
+    socket.removeAllListeners();
+    socket.close();
+    read_login.close();
+    read_input.close();
+})
 
-// print output from server
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-});
-
-// send command line input to server
-read_input.on('line', (input) => {
-    if(input === 'exit') {
-        readline.close();
-        socket.close();
+// listen for login success or failure
+socket.addEventListener('message', function(event) {
+    if(event.login_success) {
+        console.log("Login Successful");
+        socket.removeListener('message');
+        start_playing();
+    } else {
+        login();
     }
-    if(username) {
-        socket.emit('sound', {user: username, msg: input});
-    }
-});
+})
--- server.js
+++ server.js
@@ -3,21 +3,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function (socket){
-  console.log('connection');
+  console.log('new connection: ' + socket.id);
 
-  socket.on('sound', function (data) {
-    console.log('MSG', data['user'], ' saying ', data['msg']);
-    socket.send({data: "I hear you"});
+  socket.on('message', function (data) {
+    if(data['msg'] === 'exit') {
+      socket.send({data: "bye"});
+      socket.close();
+    } else {
+      console.log('MSG', socket.id, ' saying ', data['msg']);
+      socket.send({data: "I hear you"});
+    }
   });
 
-  socket.on('sight', function (data) {
-    console.log('MSG', data['user'], ' saying ', data['msg']);
-    socket.send({data: "I hear you"});
-  });
-
-  socket.on('action', function (data) {
-    console.log('MSG', data['user'], ' saying ', data['msg']);
-    socket.send({data: "I hear you"});
+  socket.on('login', function (data) {
+    console.log('login successful: ' + socket.id)
+    socket.send({login_success: true})
   });
 });
 
