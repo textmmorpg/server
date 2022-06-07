e8c904636322221db0e795a7cd1f28cb1d2db3ba
Nate Schultz <ngs32@drexel.edu>
2022-06-07 16:44:12 -0400

    worked on biome crud funcs

---
+++ server/world_generator.js
@@ -0,0 +1,16 @@
+
+const crud = require('./crud');
+async function generate() {
+    try {
+        await crud.reset_world();
+        await crud.add_biome(-10,-10,10,10,'forest');
+        await crud.add_biome(10,-10,10,10,'grassy field');
+        console.log("done");
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+generate()
