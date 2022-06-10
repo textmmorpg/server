4b2bea611ef28af087495b3b372cd77beb13d690
Nate Schultz <ngs32@drexel.edu>
2022-06-10 14:46:06 -0400

    generalized gaussian filter

--- server/world/terrain_generator.js
+++ server/world/terrain_generator.js
@@ -16,13 +16,42 @@ function randomGreyHex(value) {
 
 var noise = perlin.generatePerlinNoise(width, height);
 
+// convert to 2d array
+var counter = 0;
+var values = new Array();
+for(var lat = 0; lat < width; lat++) {
+  values.push(new Array(height));
+  for(var long = 0; long < height; long++) {
+    values[lat][long] = noise[counter];
+    counter += 1;
+  }
+}
+
+// apply gaussian blur
+function blur(size) {
+  var border = Math.floor(size/2);
+  for(var lat = border; lat < width-border; lat++) {
+      for(var long = border; long < height-border; long++) {
+          // create filter of size <size>
+          var filter = new Array();
+          for(var x = -1*border; x < border+1; x++) {
+            for(var y = -1*border; y < border+1; y++) {
+              filter.push(values[lat+x][long+y]);
+            }
+          }
+          // assign cell the average size of neighbors
+          values[lat][long] = filter.reduce((a, b) => a + b, 0)/filter.length;
+      }
+  }
+}
+blur(5);
+
+
 // write output to canvas
-var counter = 0
 for(var lat = 0; lat < width; lat++) {
   for(var long = 0; long < height; long++) {
-      context.fillStyle = randomGreyHex(noise[counter]);
+      context.fillStyle = randomGreyHex(values[lat][long]);
       context.fillRect(lat, long, 1, 1);
-      counter += 1;
   }
 }
 
