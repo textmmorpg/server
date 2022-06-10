0086750054e1b4f5c83cfced78f77881c9b6597b
Nate Schultz <ngs32@drexel.edu>
2022-06-10 13:47:08 -0400

    worked on rotation matrix

--- utils/vector.js
+++ utils/vector.js
@@ -1,9 +1,23 @@
+const { Vector } = require("simplevectorsjs");
+
 const dot_prod = (v1, v2) => v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 const cross_prod = (v1, v2) => (
     {x: v1.y * v2.z - v1.z * v2.y,
     y: v1.z * v2.x - v1.x * v2.z,
     z: v1.x * v2.y - v1.y * v2.x}
 );
+const determinant = m => 
+  m.length == 1 ?
+  m[0][0] :
+  m.length == 2 ? 
+  m[0][0]*m[1][1]-m[0][1]*m[1][0] :
+  m[0].reduce((r,e,i) => 
+    r+(-1)**(i+2)*e*determinant(m.slice(1).map(c => 
+      c.filter((_,j) => i != j))),0)
+
+var lat = Math.PI/2;
+var long = Math.PI/23;
+var height = 1;
 
 // https://en.wikipedia.org/wiki/Spherical_coordinate_system
 // generate the line element for a displacement from
@@ -14,6 +28,22 @@ const cross_prod = (v1, v2) => (
 // ^lat    = cos(lat)*cos(long)*^x + cos(lat)*sin(long)*^y - sin(lat)*^z
 // ^long   = -sin(long)*^x + cos(long)*^y
 // where ^x, ^y, ^z are the unit vectors in cartesian coordinates
+
+var dx = [1, 2, 3];
+var dy = [1, 2, 3];
+var dz = [1, 2, 3];
+
+cost vector_mult = (const)
+
+var r = Math.sin(lat)*Math.cos(long)
+
+const rotation_matrix = (lat, long, height) => {
+    return [
+        [Math.sin(lat)*Math.cos(long), Math.cos(lat)*Math.cos(long), -1*Math.sin(long)],
+        [Math.sin(lat)*Math.sin(long), Math.cos(lat)*Math.sin(long), Math.cos(long)],
+        [Math.cos(long), -1*Math.sin(long), 0]
+    ];
+}
 
 // In our case, height remains constant when moving around the sphere.
 // Perhaps when height maps/terrain on the sphere is added this will
@@ -26,7 +56,7 @@ const cross_prod = (v1, v2) => (
 // long to long + dlong on a spherical surface at constant radius height
 // https://wikimedia.org/api/rest_v1/media/math/render/svg/80362b061d72199228f100923ad7957e340bcef4
 // with height = 1
-// = |cross(^lat, sin(lat*^long))|
+// = det(cross(^lat, sin(lat*^long)))
 
 // However! If we want to use terrain then height does not remain constant
 // so we want to find the *volume element* spanning from height to height + dheight,
@@ -35,11 +65,11 @@ const cross_prod = (v1, v2) => (
 // https://wikimedia.org/api/rest_v1/media/math/render/svg/5b5b793dc864c0016f83bd137ebb9a4720d80d94
 // https://wikimedia.org/api/rest_v1/media/math/render/svg/858b36ce1577f1133ecd96c49b15b7a22fd44c29
 
-var lat = Math.PI;
-var long = Math.PI;
-var height = 1;
-const jacobian_matrix = (lat, long, height) => {return [
-    [Math.sin(lat)*Math.cos(long), height*Math.cos(lat)*Math.cos(long), -1*height*Math.sin(lat)*Math.sin(long)],
-    [Math.sin(lat)*Math.sin(long), height*Math.cos(lat)*Math.sin(long), height*Math.sin(lat)*Math.cos(long)],
-    [Math.cos(lat), -1*height*Math.sin(lat), 0]
-]};
+// const jacobian_matrix = (lat, long, height) => {return [
+//     [Math.sin(lat)*Math.cos(long), height*Math.cos(lat)*Math.cos(long), -1*height*Math.sin(lat)*Math.sin(long)],
+//     [Math.sin(lat)*Math.sin(long), height*Math.cos(lat)*Math.sin(long), height*Math.sin(lat)*Math.cos(long)],
+//     [Math.cos(lat), -1*height*Math.sin(lat), 0]
+// ]};
+
+// var dV = determinant(jacobian_matrix(lat, long, height));
+// console.log(dV);
