863b1a2e66a6c26be0ed17e53e6d268988d8978e
Nate Schultz <ngs32@drexel.edu>
2022-06-10 18:40:49 -0400

    trim border

--- server/world/terrain_generator.js
+++ server/world/terrain_generator.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const { createCanvas } = require('canvas');
+var perlin = require('perlin-noise');
 
-const width = 50;
-const height = 50;
+const width = 300;
+const height = 300;
 const canvas = createCanvas(width, height);
 const context = canvas.getContext('2d');
 
@@ -13,84 +14,88 @@ function greyHex(value) {
   return "#" + v + v + v;
 }
 
+var noise = perlin.generatePerlinNoise(width, height);
+
 // create 2d array for the heightmap
 var values = new Array();
+var counter = 0;
 for(var lat = 0; lat < width; lat++) {
   values.push(new Array(height));
   for(var long = 0; long < height; long++) {
-    values[lat][long] = 0.5
+    if(Math.random > 0.99) {
+        values[lat][long] = noise[counter]
+    } else {
+        values[lat][long] = 0
+    }
+    counter += 1;
   }
-}
-
-function pairwise(array) {
-    var result = new Array();
-    array.forEach( (point1) => {
-        array.forEach( (point2) => {
-            if (point1 !== point2) {
-                result.push([point1, point2]);
-            }
-        });
-    });
-    // remove duplicates like (x,y) and (y,x)
-    result.forEach( (pair1, i1) => {
-        result.forEach( (pair2, i2) => {
-            if(pair1[0] === pair2[1] && pair1[1] === pair2[0]) {
-                result.splice(i1, 1);
-            }
-        });
-    });
-    return result;
 }
 
 function midpoint(point1, point2) {
     return [Math.floor((point1[0]+point2[0])/2), Math.floor((point1[1]+point2[1])/2)];
 }
 
-function multiDimensionalUnique(arr) {
-    var uniques = [];
-    var itemsFound = {};
-    for(var i = 0, l = arr.length; i < l; i++) {
-        var stringified = JSON.stringify(arr[i]);
-        if(itemsFound[stringified]) { continue; }
-        uniques.push(arr[i]);
-        itemsFound[stringified] = true;
+function create_ridge(points, iter) {
+    if(iter <= 0) return;
+    var cur_midpoint = midpoint(points[0], points[1]);
+    var h = (values[points[0][0]][points[0][1]] + values[points[1][0]][points[1][1]])/2;
+    var new_h = h + (Math.random()-0.5);
+    // if(new_h > 1) new_h = 1;
+    // if(new_h < 0) new_h = 0;
+    values[cur_midpoint[0]][cur_midpoint[1]] = new_h
+    // iterate with new midpoints recursively
+    var new_points = [
+        midpoint(points[0], cur_midpoint),
+        midpoint(points[1], cur_midpoint)
+    ]
+    create_ridge(new_points, iter-1);
+}
+
+for(var x = 0; x < width; x++) {
+    for(var y = 0; y < height; y++) {
+        if(Math.random() > 0.5) create_ridge([[x, y], [0, 0]], 30)
+        if(Math.random() > 0.5) create_ridge([[x, y], [width-1, height-1]], 30)
+        if(Math.random() > 0.5) create_ridge([[x, y], [width-1, 0]], 30)
+        if(Math.random() > 0.5) create_ridge([[x, y], [0, height-1]], 30)
+        if(Math.random() > 0.5) create_ridge([[x, y], [
+            Math.floor(Math.random()*(width-1)), 
+            Math.floor(Math.random()*(height-1))
+        ]], 30)
     }
-    return uniques;
 }
 
-// using a modified diamond square algorithm
-points = [[0,0],[0,height-1],[width-1,0],[width-1,height-1]];
-function get_all_midpoints(points) {
-    // find all the midpoints
-    var midpoints = new Array();
-    // and store the average heights of the two points that generated them
-    var heights = new Array();
-    pairwise(points).forEach( (pair) => {
-        height_avg = (values[pair[0][0]][pair[0][1]] + values[pair[1][0]][pair[1][1]])/2;
-        midpoints.push(midpoint(pair[0], pair[1]));
-        heights.push(height_avg);
-    });
-    // todo: remove duplicates before returning
-    return [midpoints, heights];
-}
+// apply gaussian blur
+function blur(size) {
+    var new_values = values;
+    var border = Math.floor(size/2);
+    for(var lat = border; lat < width-border; lat++) {
+        for(var long = border; long < height-border; long++) {
+            // create filter of size <size>
+            var filter = new Array();
+            for(var x = -1*border; x < border+1; x++) {
+              for(var y = -1*border; y < border+1; y++) {
+                filter.push(values[lat+x][long+y]);
+              }
+            }
+            // assign cell the average size of neighbors
+            new_val = filter.reduce((a, b) => a + b, 0)/filter.length;
+            if(new_val > 1) new_val = 0.999;
+            if(new_val < 0) new_val = 0.001;
+            new_values[lat][long] = new_val;
+        }
+    }
 
-function update_heights(points, heights) {
-    points.forEach( (point, index) => {
-        var new_height = heights[index] + (Math.random()-0.5)*0.2
-        values[point[0]][point[1]] = new_height;
-    })
+    return new_values;
 }
-
-var iter_count = 3;
-for(var i = 0; i < iter_count; i++) {
-    var midpoint_heights = get_all_midpoints(points);
-    update_heights(midpoint_heights[0], midpoint_heights[1])
-    points = midpoint_heights[0].concat(points);
-}
+values = blur(15);
+values = blur(3);
+values = blur(15);
+values = blur(15);
 
 // write output to canvas
-for(var lat = 0; lat < width; lat++) {
-  for(var long = 0; long < height; long++) {
+var border = Math.floor(21/2)
+for(var lat = border; lat < width-border; lat++) {
+  for(var long = border; long < height-border; long++) {
       context.fillStyle = greyHex(values[lat][long]);
       context.fillRect(lat, long, 1, 1);
   }
