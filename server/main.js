92a38e0387f1d496c82d183320cbde05d9b370f2
Nate Schultz <ngs32@drexel.edu>
2022-06-11 11:30:24 -0400

    updated announcements

--- server/main.js
+++ server/main.js
@@ -84,7 +84,7 @@ io.on('connection', function (socket){
 
   socket.on('turn slight left', function(data) {
     crud_move.move(socket, 0, Math.PI/6);
-    announce.announce(socket.id, io, 'turned left', seeing_distance, true);
+    announce.announce(socket.id, io, 'turned slight left', seeing_distance, true);
   });
 
   socket.on('turn left', function(data) {
@@ -94,12 +94,12 @@ io.on('connection', function (socket){
 
   socket.on('turn hard left', function(data) {
     crud_move.move(socket, 0, Math.PI/2);
-    announce.announce(socket.id, io, 'turned left', seeing_distance, true);
+    announce.announce(socket.id, io, 'turned hard left', seeing_distance, true);
   });
 
   socket.on('turn slight right', function(data) {
     crud_move.move(socket, 0, Math.PI/6 * -1);
-    announce.announce(socket.id, io, 'turned right', seeing_distance, true);
+    announce.announce(socket.id, io, 'turned slight right', seeing_distance, true);
   });
 
   socket.on('turn right', function(data) {
@@ -109,7 +109,7 @@ io.on('connection', function (socket){
 
   socket.on('turn hard right', function(data) {
     crud_move.move(socket, 0, Math.PI/2 * -1);
-    announce.announce(socket.id, io, 'turned right', seeing_distance, true);
+    announce.announce(socket.id, io, 'turned hard right', seeing_distance, true);
   });
 
   socket.on('turn around', function(data) {
