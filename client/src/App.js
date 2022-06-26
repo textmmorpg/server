aea46aa767aa3b55a6e801ca128ca5211e36cd9b
Nate Schultz <ngs32@drexel.edu>
2022-06-26 17:38:50 -0400

    add chats

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   Button,
@@ -39,40 +39,55 @@ const AppBar = (props) => (
   />
 );
 
-const Messages = () => {
-  var message_count = 50;
-  var ret;
-  for(var i = 0; i < message_count; i++) {
-    ret = [ret,
-      <Box pad={{horizontal: "xlarge", top: "large"}}>
-        <Tag name="name" value="value" />
-      </Box>
-    ];
-  }
-  return ret;
-}
-
-const InputBox = () => {
-  const [value, setValue] = React.useState('');
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      setValue('');
-    }
-  }
-
-  return (
-    <TextInput
-      placeholder="type here"
-      value={value}
-      onChange={event => setValue(event.target.value)}
-      onKeyDown={handleKeyDown}
-    />
-  );
-}
-
 function App() {
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [showSidebar, setShowSidebar] = useState('');
+  const [value, setValue] = useState('');
+  const [messages, setMessages] = useState(() => {
+    return [{
+      name:"Server", value:"This is a multiplayer text adventure game! " +
+      "Type commands to interact with the world and other players. " +
+      "For example, type 'walk forward' to walk, 'turn right' to turn, etc. " +
+      "Use 'say hello' to say hello to players around you. Use 'look' or 'vibe check' to " + 
+      "examine your immediate environment. For the full list of commands, " +
+      "check the wiki: https://github.com/beefy/textmmo/wiki"
+    }];
+  });
+
+  useEffect(() => {
+    console.log(messages)
+  });
+
+  const Messages = () => {
+    var ret;
+    for(var i = 0; i < messages.length; i++) {
+      ret = [ret,
+        <Box pad={{horizontal: "xlarge", top: "large"}}>
+          <Tag border={false} name={messages[i]['name']} value={messages[i]["value"]} />
+        </Box>
+      ];
+    }
+    return ret;
+  }
+
+  const InputBox = () => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        setMessages(messages.concat([{name: "You", value: value}]));
+        setValue('');
+      }
+    }
+  
+    return (
+      <TextInput
+        key="input"
+        placeholder="type here"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    );
+  }
+  
 
   return (
     <Grommet theme={theme} full>
@@ -89,10 +104,10 @@ function App() {
               <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
                 <Box fill>
                   <Box overflow={{vertical: "scroll"}}>
-                    <Messages />
+                    <Messages/>
                   </Box>
                   <Box pad={{horizontal: "medium", bottom: "large"}}>
-                    <InputBox />
+                    {InputBox()}
                   </Box>
                 </Box>
                 {(!showSidebar || size !== 'small') ? (
